d144e48390a41b14e52442adbb1de549822959cd
James Vickery <dev@jamesvickery.net>
2018-02-04 00:11:34 +0000

    Added test film stuff and made it pretty

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-angular.module('filmderApp', ['gajus.swing'])
+angular.module('filmderApp', [])
   .controller('FilderController', function ($scope) {
     var filmder = this;
+
+    var testImg = 'http://via.placeholder.com/150x200'
+
+    $scope.filmQueue = [{
+      title: 'Wolf of Wall Street',
+      desc: 'Money n stuff?',
+      year: '2012',
+      img: testImg
+
+    }, {
+      title: 'Titanic',
+      desc: 'This boat won\'t sink tbh. Wait fuck where alla this water come from bihhh',
+      year: '2019',
+      img: testImg
+    }, {
+      title: 'Monsters, Inc',
+      desc: 'Scary roar lmao',
+      year: '1948',
+      img: testImg
+    }];
+
+    $scope.currentFilm = $scope.filmQueue.pop();
+    $scope.getNextFilm = function () {
+      currentFilm = $scope.filmQueue.pop();
+    }
 
   });
