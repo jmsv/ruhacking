f9f5f5e756a5a0ce1d8ff0fb0d8dd83690c8a046
James Vickery <dev@jamesvickery.net>
2018-02-04 11:26:58 +0000

    Last bits of stuff

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
 angular.module('filmderApp', [])
-  .controller('FilderController', function ($scope) {
+  .controller('FilderController', function ($scope, $http) {
     var filmder = this;
 
     var testImg = 'http://via.placeholder.com/150x200'
 
     $scope.filmQueue = [{
-      title: 'Wolf of Wall Street',
-      desc: 'Money n stuff?',
-      year: '2012',
-      img: testImg
-
-    }, {
-      title: 'Titanic',
-      desc: 'This boat won\'t sink tbh. Wait fuck where alla this water come from bihhh',
-      year: '2019',
-      img: testImg
-    }, {
-      title: 'Monsters, Inc',
-      desc: 'Scary roar lmao',
-      year: '1948',
-      img: testImg
+      "length": "97",
+      "cast": "Ellen DeGeneres, Albert Brooks,Ed O'Neill, Kaitlin Olson",
+      "year": "2016",
+      "title": "Finding Dory",
+      "desc": "The friendly but forgetful blue tang fish, Dory, begins a search for her long-lost parents, and everyone learns a few things about the real meaning of family along the way.",
+      "genre": "Animation,Adventure,Comedy",
+      "trailer_url": "https://www.youtube.com/watch?v=0tkLUap7oGQ"
     }];
+
+    $http.get('fake-response.json')
+      .then(function (res) {
+        var data = res.data;
+        for (var i = 0; i < data.length; i++) {
+          data[i].trailer_url = "https://www.youtube.com/watch?v=" + data[i].trailer_url;
+          $scope.filmQueue.push(data[i])
+        }
+
+        console.log($scope.filmQueue)
+      });
 
     $scope.currentFilm = $scope.filmQueue.pop();
     $scope.getNextFilm = function () {
-      currentFilm = $scope.filmQueue.pop();
+      console.log("click");
+      $scope.currentFilm = $scope.filmQueue.pop();
     }
 
   });
